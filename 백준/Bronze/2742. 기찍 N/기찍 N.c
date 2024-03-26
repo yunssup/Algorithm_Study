f395c38638a77724a02bab7497e3528b37e2dd54
yunssup <128561564+yunssup@users.noreply.github.com>
2024-03-26 17:36:50 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main (void){
+    
+    int N;
+    
+    scanf ("%d",&N);
+    
+    for(int j=N; j>0; j--){
+        printf ("%d\n",j);
+    }
+}
