73acfd0c4999cefc534076ace109f90d7cf87684
yunssup <128561564+yunssup@users.noreply.github.com>
2024-03-27 16:29:21 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int i,j;
+    scanf("%d",&n);
+
+    for(i=1; i<=n; i++) { // n번 줄 반복하는 for 문
+        
+            for(j=1; j<=n-i; j++) // 공백 출력 위한 for문
+            printf(" ");
+        for (j=1; j<=i; j++) 
+            printf("*");
+
+        printf("\n");
+    }
+    for(i=n-1; i>=1; i--){
+                    for(j=1; j<=n-i; j++) // 공백 출력 위한 for문
+            printf(" ");
+           for (j=1; j<=i; j++) // 왼쪽 * 출력 for문
+                printf("*");
+
+    printf("\n");
+    }
+    return 0;
+}
